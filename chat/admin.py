f75f8045335d665f443df7172555c6e5ca4cd8ae
Jeon752 <7497330@naver.com>
2024-11-24 15:44:36 +0900

    Remove unnecessary files and add new changes

--- chat/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import ChatRoom
-
-@admin.register(ChatRoom)
-class ChatRoomAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')  # 관리자 페이지에서 표시할 필드
